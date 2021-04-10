31ec764ede612cc330728af8ea414472cb4e67ec
Patricio Di Muzio <dimuziop@gmail.com>
2021-04-10 12:29:51 +0200

    feature: actors and BPs

--- src/main/scala/part2actors/ActorsIntro.scala
+++ src/main/scala/part2actors/ActorsIntro.scala
@@ -1,6 +1,6 @@
 package part2actors
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
 /**
  * User: patricio
@@ -13,5 +13,48 @@ object ActorsIntro extends App {
 
   val actorSystem = ActorSystem("firstActorSystem")
   println(actorSystem.name)
+
+  // part 2 - create actors
+  // word count actor
+
+  class WordCountActor extends Actor {
+    // internal data
+    var totalWords = 0
+
+    // behavior
+    override def receive: Receive = {
+      case message: String =>
+        println(s"[word counter] I have receive $message")
+        totalWords += message.split(" ").length
+      case meg => println(s"I cant understand ${meg.toString}")
+    }
+  }
+
+  // part3 - instantiate our actor
+  val wordCounter: ActorRef = actorSystem.actorOf(Props[WordCountActor], "wordCounter")
+  val anotherWordCounter: ActorRef = actorSystem.actorOf(Props[WordCountActor], "anotherWordCounter")
+
+  //part4 - communicate
+  wordCounter ! "I'm learning Akka and it's a pretty damn cool" // "tell"
+  anotherWordCounter ! "A different message"
+  //asynchronous
+
+  object Person {
+    def props(name: String): Props = Props(new Person(name))
+  }
+
+  class Person(name: String) extends Actor {
+    override def receive: Receive = {
+      case "hi" => println(s"Hi my name is $name")
+      case _ =>
+    }
+  }
+
+  val person = actorSystem.actorOf(Person.props("Bob"))
+
+  person ! "hi"
+
+
+
 
 }
