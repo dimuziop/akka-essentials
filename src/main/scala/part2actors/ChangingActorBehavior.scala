c483bad84d2eeb35d55b8216597fae42cad4aaff
Patricio Di Muzio <dimuziop@gmail.com>
2021-04-13 13:27:56 +0200

    fixing error and testing with other imp

--- src/main/scala/part2actors/ChangingActorBehavior.scala
+++ src/main/scala/part2actors/ChangingActorBehavior.scala
@@ -155,12 +155,12 @@ object ChangingActorBehavior extends App {
     override def receive: Receive = poll(None)
 
     def poll(name: Option[String]): Receive = {
-      case Vote(candidate) => context.become(poll(Option(candidate)))
+      case Vote(candidate) => context.become(poll(Some(candidate)))
       case VoteStatusRequest => sender() ! VoteStatusReply(name)
     }
   }
 
-  case class AggregateVotes(citizens: Set[ActorRef])
+  /*case class AggregateVotes(citizens: Set[ActorRef])
 
   class VoteAggregator extends Actor {
 
@@ -171,19 +171,46 @@ object ChangingActorBehavior extends App {
         if (candidate.isEmpty) context.become(pollOpen(value))
         else {
           if (!value.contains(candidate.get)) {
-            context.become(pollOpen(value ++ Map((candidate.get, 1))))
+            println(candidate.get + " new")
+            context.become(pollOpen(value + (candidate.get -> 1)))
           }
-          else  {
-            context.become(pollOpen(value ++ Map((candidate.get, value(candidate.get) + 1))))
+          else {
+            println(candidate.get + " adsd")
+            context.become(pollOpen(value + (candidate.get -> (value(candidate.get) + 1))))
           }
         }
       case AggregateVotes(voters: Set[ActorRef]) =>
-        if (voters.isEmpty) {
-          Thread.sleep(1000)
-          println(value)
-        } else {
+        if (voters.nonEmpty) {
           voters.head ! VoteStatusRequest
           self ! AggregateVotes(voters.tail)
+        }
+        println(value)
+    }
+  }*/
+
+  case class AggregateVotes(citizens: Set[ActorRef])
+  class VoteAggregator extends Actor {
+    override def receive: Receive = awaitingCommand
+
+    def awaitingCommand: Receive = {
+      case AggregateVotes(citizens) =>
+        citizens.foreach(citizenRef => citizenRef ! VoteStatusRequest)
+        context.become(awaitingStatuses(citizens, Map()))
+    }
+
+    def awaitingStatuses(stillWaiting: Set[ActorRef], currentStats: Map[String, Int]): Receive = {
+      case VoteStatusReply(None) =>
+        // a citizen hasn't voted yet
+        sender() ! VoteStatusRequest // this might end up in an infinite loop
+      case VoteStatusReply(Some(candidate)) =>
+        val newStillWaiting = stillWaiting - sender()
+        val currentVotesOfCandidate = currentStats.getOrElse(candidate, 0)
+        val newStats = currentStats + (candidate -> (currentVotesOfCandidate + 1))
+        if (newStillWaiting.isEmpty) {
+          println(s"[aggregator] poll stats: $newStats")
+        } else {
+          // still need to process some statuses
+          context.become(awaitingStatuses(newStillWaiting, newStats))
         }
     }
   }
